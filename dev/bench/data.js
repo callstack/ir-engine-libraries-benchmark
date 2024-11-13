4afda221caed2d24e39632c90d4a1e138c44eba7
github-action-benchmark <github@users.noreply.github.com>
2024-11-13 22:51:23 +0000

    add React Native Benchmarks (customSmallerIsBetter) benchmark result for 411422a76c8d8f0dda70ac24682aa768426de5a3

---
+++ dev/bench/data.js
@@ -0,0 +1,78 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1731538283337,
+  "repoUrl": "https://github.com/callstack/ir-engine-libraries-benchmark",
+  "entries": {
+    "React Native Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "callstack",
+            "username": "callstack"
+          },
+          "committer": {
+            "name": "callstack",
+            "username": "callstack"
+          },
+          "id": "411422a76c8d8f0dda70ac24682aa768426de5a3",
+          "message": "Benchmarking Github Actions setup",
+          "timestamp": "2024-11-12T16:51:34Z",
+          "url": "https://github.com/callstack/ir-engine-libraries-benchmark/pull/2/commits/411422a76c8d8f0dda70ac24682aa768426de5a3"
+        },
+        "date": 1731538282537,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "jsc/Apple iPhone 15 Pro/Basis: File Encoding To KTX2WallClock-ms.txt",
+            "value": 62933,
+            "unit": "ms"
+          },
+          {
+            "name": "jsc/Apple iPhone 15 Pro/Basis: File Transcoding From .basisWallClock-ms.txt",
+            "value": 1533,
+            "unit": "ms"
+          },
+          {
+            "name": "jsc/Apple iPhone 15 Pro/Draco: Decoding + Attribute accessWallClock-ms.txt",
+            "value": 138,
+            "unit": "ms"
+          },
+          {
+            "name": "jsc/Apple iPhone 15 Pro/Draco: DecodingWallClock-ms.txt",
+            "value": 56,
+            "unit": "ms"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/Basis: File Encoding To KTX2WallClock-ms.txt",
+            "value": 64570,
+            "unit": "ms"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/Basis: File Transcoding From .basisWallClock-ms.txt",
+            "value": 1661,
+            "unit": "ms"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/Draco: Decoding + Attribute accessWallClock-ms.txt",
+            "value": 143,
+            "unit": "ms"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/Draco: DecodingWallClock-ms.txt",
+            "value": 94,
+            "unit": "ms"
+          },
+          {
+            "name": "Bundle Size",
+            "value": 1000,
+            "unit": "kiB"
+          },
+          {
+            "name": "Executable Size",
+            "value": 19156,
+            "unit": "kiB"
+          }
+        ]
+      }
+    ]
+  }
+}
