28fc9f46c31af5a46e4e816b639509e5ed0774c1
github-action-benchmark <github@users.noreply.github.com>
2024-11-14 15:21:10 +0000

    add React Native Benchmarks (customSmallerIsBetter) benchmark result for e83d047d86f41e172dca5782bd92e71987eddef5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731538283337,
+  "lastUpdate": 1731597670189,
   "repoUrl": "https://github.com/callstack/ir-engine-libraries-benchmark",
   "entries": {
     "React Native Benchmarks": [
@@ -59,6 +59,80 @@ window.BENCHMARK_DATA = {
           {
             "name": "hermes/Apple iPhone 15 Pro/Draco: DecodingWallClock-ms.txt",
             "value": 94,
+            "unit": "ms"
+          },
+          {
+            "name": "Bundle Size",
+            "value": 1000,
+            "unit": "kiB"
+          },
+          {
+            "name": "Executable Size",
+            "value": 19156,
+            "unit": "kiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "31718268+hmallen99@users.noreply.github.com",
+            "name": "Henry Allen",
+            "username": "hmallen99"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e83d047d86f41e172dca5782bd92e71987eddef5",
+          "message": "Benchmarking Github Actions setup (#2)\n\n* Copy appium server code\r\n\r\n* Update benchmark script with draco and basis\r\n\r\n* Add benchmark parser\r\n\r\n* Clean up files\r\n\r\n* Move to packages\r\n\r\n* Add benchmark workflow\r\n\r\n* Reorganize benchmark time text\r\n\r\n* Update gitignore\r\n\r\n* Move scripts to separate folder\r\n\r\n* delete unzipZrchive\r\n\r\n* Hide test artifacts\r\n\r\n* Update benchmark.yaml references\r\n\r\n* Update paths for benchmark scripts\r\n\r\n* Remove jest config\r\n\r\n* Temporarily add PR workflow\r\n\r\n* Add build commands\r\n\r\n* Fix application location\r\n\r\n* Fix lock\r\n\r\n* Add actions: read\r\n\r\n* Bump timeout\r\n\r\n* Use npm package lock as source of truth\r\n\r\n* Remove yarn.lock\r\n\r\n* Remove PR workflow",
+          "timestamp": "2024-11-14T09:24:29-05:00",
+          "tree_id": "62c2caf5fcbc4949ebc5c487eba9c7b70d19d266",
+          "url": "https://github.com/callstack/ir-engine-libraries-benchmark/commit/e83d047d86f41e172dca5782bd92e71987eddef5"
+        },
+        "date": 1731597668989,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "jsc/Apple iPhone 15 Pro/Basis: File Encoding To KTX2WallClock-ms.txt",
+            "value": 58130,
+            "unit": "ms"
+          },
+          {
+            "name": "jsc/Apple iPhone 15 Pro/Basis: File Transcoding From .basisWallClock-ms.txt",
+            "value": 1495,
+            "unit": "ms"
+          },
+          {
+            "name": "jsc/Apple iPhone 15 Pro/Draco: Decoding + Attribute accessWallClock-ms.txt",
+            "value": 155,
+            "unit": "ms"
+          },
+          {
+            "name": "jsc/Apple iPhone 15 Pro/Draco: DecodingWallClock-ms.txt",
+            "value": 68,
+            "unit": "ms"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/Basis: File Encoding To KTX2WallClock-ms.txt",
+            "value": 66762,
+            "unit": "ms"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/Basis: File Transcoding From .basisWallClock-ms.txt",
+            "value": 1665,
+            "unit": "ms"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/Draco: Decoding + Attribute accessWallClock-ms.txt",
+            "value": 127,
+            "unit": "ms"
+          },
+          {
+            "name": "hermes/Apple iPhone 15 Pro/Draco: DecodingWallClock-ms.txt",
+            "value": 89,
             "unit": "ms"
           },
           {
